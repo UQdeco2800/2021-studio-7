4ca04ba4b8dff787a964b594820d4a055c4740b6
Ben Jackson <bjen148@gmail.com>
2021-08-24 23:52:11 +1000

    Started implementing an event system into InteractableComponent

--- source/core/src/main/com/deco2800/game/components/InteractableComponent.java
+++ source/core/src/main/com/deco2800/game/components/InteractableComponent.java
@@ -1,6 +1,13 @@
 package com.deco2800.game.components;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.physics.BodyUserData;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.components.HitboxComponent;
+import com.deco2800.game.physics.components.PhysicsComponent;
 
 /**
  * When this entity collides with the player's hitbox, triggers an event, and
@@ -11,6 +18,10 @@ import com.badlogic.gdx.physics.box2d.Fixture;
  */
 public class InteractableComponent extends Component {
     private short targetLayer;
+    private String collisionEvent;
+    private String interactionEvent;
+    private HitboxComponent hitboxComponent;
+    private boolean isTouching = false;
 
     /**
      * Create a component which listens for collisions with the player on its
@@ -20,6 +31,7 @@ public class InteractableComponent extends Component {
      */
     public InteractableComponent (short targetLayer, String collisionEvent) {
         this.targetLayer = targetLayer;
+        this.collisionEvent = collisionEvent;
     }
 
     /**
@@ -34,17 +46,52 @@ public class InteractableComponent extends Component {
     public InteractableComponent (short targetLayer, String collisionEvent,
                                   String interactionEvent) {
         this.targetLayer = targetLayer;
+        this.collisionEvent = collisionEvent;
+        this.interactionEvent = interactionEvent;
     }
 
     @Override
     public void create() {
         entity.getEvents().addListener("collisionStart",
                 this::onCollisionStart);
+        entity.getEvents().addListener("collisionEnd",
+                this::onCollisionEnd);
         // TODO Collision start will currently trigger with both the player AND
-        //  NPCs. We want it to trigger on just the NPCS
+        //  NPCs. We want it to trigger on just the player, NOT the NPCS.
+        hitboxComponent = entity.getComponent(HitboxComponent.class);
     }
 
     private void onCollisionStart(Fixture me, Fixture other) {
+        if (hitboxComponent.getFixture() != me) {
+            // Not triggered by hitbox, ignore
+            return;
+        }
 
+        if (!PhysicsLayer.contains(targetLayer,
+                other.getFilterData().categoryBits)) {
+            // Doesn't match our target layer, ignore
+            return;
+        }
+
+        this.isTouching = true;
+        // TODO Sprite changes and trigger event
+
+        // Doesn't do anything yet
+        entity.getEvents().trigger("interactionStart");
+    }
+
+    private void onCollisionEnd(Fixture me, Fixture other) {
+        this.isTouching = false;
+        // TODO Undo sprite changes
+
+        // Doesn't do anything yet
+        entity.getEvents().trigger("interactionEnd");
+    }
+
+    private void onInteraction(Fixture me, Fixture other) {
+        if (!isTouching) {
+            return;
+        }
+        // TODO stuff that happens when interacted with
     }
 }
