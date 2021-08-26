2a01e77eeba2a5c88d81b1a00ad2d2defcd4050f
Jantoom <75770311+Jantoom@users.noreply.github.com>
2021-08-27 01:03:01 +1000

    Edited EndGameScreen to appear more in-line with MainMenuScreen.

--- source/core/src/main/com/deco2800/game/screens/EndGameScreen.java
+++ source/core/src/main/com/deco2800/game/screens/EndGameScreen.java
@@ -4,23 +4,22 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.deco2800.game.GdxGame;
 import com.deco2800.game.components.endgame.EndGameActions;
+import com.deco2800.game.components.endgame.EndGameExitDisplay;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.EntityService;
 import com.deco2800.game.entities.factories.RenderFactory;
-import com.deco2800.game.input.InputComponent;
 import com.deco2800.game.input.InputDecorator;
 import com.deco2800.game.input.InputService;
 import com.deco2800.game.rendering.RenderService;
 import com.deco2800.game.rendering.Renderer;
-import com.deco2800.game.services.GameTime;
 import com.deco2800.game.services.ResourceService;
 import com.deco2800.game.services.ServiceLocator;
-import com.deco2800.game.components.endgame.EndGameExitDisplay;
-import com.deco2800.game.ui.terminal.Terminal;
-import com.deco2800.game.ui.terminal.TerminalDisplay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The win/lose screen at the end of the game.
+ */
 public class EndGameScreen extends ScreenAdapter {
     private static final Logger logger = LoggerFactory.getLogger(EndGameScreen.class);
     private static final String[] winScreenTextures = {"images/win_screen_bg.png"};
@@ -35,16 +34,12 @@ public class EndGameScreen extends ScreenAdapter {
         this.result = result;
 
         logger.debug("Initialising end game screen services");
-        ServiceLocator.registerTimeSource(new GameTime());
-
         ServiceLocator.registerInputService(new InputService());
         ServiceLocator.registerResourceService(new ResourceService());
-
         ServiceLocator.registerEntityService(new EntityService());
         ServiceLocator.registerRenderService(new RenderService());
 
         renderer = RenderFactory.createRenderer();
-        renderer.getCamera().getEntity().setPosition(5f, 5f);
 
         loadAssets();
         createUI();
@@ -59,6 +54,17 @@ public class EndGameScreen extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         renderer.resize(width, height);
+        logger.trace("Resized renderer: ({} x {})", width, height);
+    }
+
+    @Override
+    public void pause() {
+        logger.info("Game paused");
+    }
+
+    @Override
+    public void resume() {
+        logger.info("Game resumed");
     }
 
     @Override
@@ -67,7 +73,6 @@ public class EndGameScreen extends ScreenAdapter {
 
         renderer.dispose();
         unloadAssets();
-
         ServiceLocator.getEntityService().dispose();
         ServiceLocator.getRenderService().dispose();
         ServiceLocator.getResourceService().dispose();
@@ -103,17 +108,10 @@ public class EndGameScreen extends ScreenAdapter {
     private void createUI() {
         logger.debug("Creating ui");
         Stage stage = ServiceLocator.getRenderService().getStage();
-        InputComponent inputComponent =
-                ServiceLocator.getInputService().getInputFactory().createForTerminal();
-
         Entity ui = new Entity();
-        ui.addComponent(new InputDecorator(stage, 10))
-                .addComponent(new EndGameActions(this.game))
-                .addComponent(new EndGameExitDisplay())
-                .addComponent(new Terminal())
-                .addComponent(inputComponent)
-                .addComponent(new TerminalDisplay());
-
+        ui.addComponent(new EndGameExitDisplay())
+                .addComponent(new InputDecorator(stage, 10))
+                .addComponent(new EndGameActions(game));
         ServiceLocator.getEntityService().register(ui);
     }
 }
