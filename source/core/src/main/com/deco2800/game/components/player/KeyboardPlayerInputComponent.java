4d181edda94ca43792fc00ce3b81b79c36e278ef
Ben Jackson <bjen148@gmail.com>
2021-08-27 02:58:25 +1000

    fixed typo in InteractableComponent

--- source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
@@ -45,6 +45,8 @@ public class KeyboardPlayerInputComponent extends InputComponent {
       case Keys.SPACE:
         entity.getEvents().trigger("attack");
         return true;
+      case Keys.E:
+        entity .getEvents().trigger("interact");
       default:
         return false;
     }
