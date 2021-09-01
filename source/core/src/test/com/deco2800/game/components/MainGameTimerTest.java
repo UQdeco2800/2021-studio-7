a77b80cc5cfd72ea9c554f106480f9b82135eafe
Roy Browning <roy6browning@gmail.com>
2021-09-01 17:59:02 +1000

    Begun timer tests

---
+++ source/core/src/test/com/deco2800/game/components/MainGameTimerTest.java
@@ -0,0 +1,13 @@
+package com.deco2800.game.components;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.deco2800.game.extensions.GameExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+public class MainGameTimerTest {
+    @Test
+
+}
