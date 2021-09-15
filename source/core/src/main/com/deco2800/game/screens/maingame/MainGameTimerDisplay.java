93a16332055822701622b48a2df7bf383e5b6a8b
XUE_wallace <xue.huang@uqconnect.edu.au>
2021-09-15 13:32:24 +1000

    elimate unused code and add log info

--- source/core/src/main/com/deco2800/game/screens/maingame/MainGameTimerDisplay.java
+++ source/core/src/main/com/deco2800/game/screens/maingame/MainGameTimerDisplay.java
@@ -5,29 +5,29 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.deco2800.game.generic.ServiceLocator;
 import com.deco2800.game.ui.components.UIComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
  * A ui component for displaying player stats, e.g. health.
  */
 public class MainGameTimerDisplay extends UIComponent {
-    Table table;
     Table timeTable;
-//    private final Label timerLabel;
     private final Label currentTimeLabel;
-    private static int timeLeft;
     private long lastTime = 0L;
     private int hour;
     private int minute;
+    private static final Logger logger =
+            LoggerFactory.getLogger(MainGameTimerDisplay.class);
 
-    public MainGameTimerDisplay(int initialTime) {
-        timeLeft = initialTime;
+    public MainGameTimerDisplay() {
+        logger.debug("Initialising main game screen timer service");
         hour = 20;
         minute = 0;
-//        CharSequence text = String.format("Time left: %ds", timeLeft);
         CharSequence timeText = String.format("%d : 0%d",hour, minute);
-//        timerLabel = new Label(text, skin, "large");
         currentTimeLabel = new Label(timeText, skin, "large");
+        logger.debug("Main game screen timer service started");
 
     }
 
@@ -45,15 +45,10 @@ public class MainGameTimerDisplay extends UIComponent {
      * @see Table for positioning options
      */
     public void addActors() {
-//        table = new Table();
-//        table.bottom().left().padBottom(10f).padLeft(5f);
         timeTable = new Table();
         timeTable.top().right().padTop(10f).padRight(120f);
-        table.setFillParent(true);
-//        timeTable.setFillParent(true);
-//        table.add(timerLabel);
+        timeTable.setFillParent(true);
         timeTable.add(currentTimeLabel);
-        stage.addActor(table);
         stage.addActor(timeTable);
     }
 
@@ -61,14 +56,6 @@ public class MainGameTimerDisplay extends UIComponent {
     public void draw(SpriteBatch batch)  {
         // draw is handled by the stage
     }
-
-    /**
-     * Updates the player's time left on the ui.
-     */
-//    public void updatePlayerTimerUI() {
-//            CharSequence text = String.format("Time left: %ds", timeLeft);
-//            timerLabel.setText(text);
-//    }
 
     /**
      * Updates the main game screen clock
@@ -105,14 +92,10 @@ public class MainGameTimerDisplay extends UIComponent {
 
     @Override
     public void dispose() {
-        table.clear();
+        logger.debug("Disposing timer");
         timeTable.clear();
         super.dispose();
     }
-
-//    public static void tick() {
-//        timeLeft = timeLeft - 1;
-//    }
 
     /**
      * Main function for timer, it would update time left
@@ -125,6 +108,7 @@ public class MainGameTimerDisplay extends UIComponent {
             lastTime = currentTime;
             updateTimeUI();
             if (hour == 2 && minute > 0) {
+                logger.info("Time end");
                 entity.getEvents().trigger("loss_timed");
             }
         }
