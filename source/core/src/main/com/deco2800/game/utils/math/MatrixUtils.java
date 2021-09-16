4625d65d6e1c8ba346126db38d5f8b3f96011a72
Jantoom <75770311+Jantoom@users.noreply.github.com>
2021-09-16 14:57:22 +1000

    Generalised MatrixUtils to accept any types for matrix manipulation.

--- source/core/src/main/com/deco2800/game/utils/math/MatrixUtils.java
+++ source/core/src/main/com/deco2800/game/utils/math/MatrixUtils.java
@@ -2,18 +2,18 @@ package com.deco2800.game.utils.math;
 
 public class MatrixUtils {
 
-    public static String[][] rotateClockwise(String[][] matrix) {
+    public static <T> T[][] rotateClockwise(T[][] matrix) {
         return flipVertically(transpose(matrix));
     }
 
-    public static String[][] rotateAntiClockwise(String[][] matrix) {
+    public static <T> T[][] rotateAntiClockwise(T[][] matrix) {
         return transpose(flipVertically(matrix));
     }
 
-    public static String[][] flipVertically(String[][] matrix) {
+    public static <T> T[][] flipVertically(T[][] matrix) {
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix.length / 2; y++) {
-                String temp = matrix[x][y];
+                T temp = matrix[x][y];
                 matrix[x][y] = matrix[x][matrix.length - y - 1];
                 matrix[x][matrix.length - y - 1] = temp;
             }
@@ -21,10 +21,10 @@ public class MatrixUtils {
         return matrix;
     }
 
-    public static String[][] flipHorizontally(String[][] matrix) {
+    public static <T> T[][] flipHorizontally(T[][] matrix) {
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix.length / 2; y++) {
-                String temp = matrix[x][y];
+                T temp = matrix[x][y];
                 matrix[x][y] = matrix[matrix.length - x - 1][y];
                 matrix[matrix.length - x - 1][y] = temp;
             }
@@ -32,14 +32,15 @@ public class MatrixUtils {
         return matrix;
     }
 
-    public static String[][] transpose(String[][] matrix) {
-        String[][] transposed = new String[matrix.length][matrix.length];
+    public static <T> T[][] transpose(T[][] matrix) {
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix.length; y++) {
-                transposed[x][y] = matrix[y][x];
+                T temp = matrix[x][y];
+                matrix[x][y] = matrix[y][x];
+                matrix[y][x] = temp;
             }
         }
-        return transposed;
+        return matrix;
     }
 
     private MatrixUtils() {
