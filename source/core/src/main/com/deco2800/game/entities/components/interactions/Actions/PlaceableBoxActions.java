a476340a5852710ba36a80d9c2709c8b35a2977f
E-Roderick <e.roderick@uqconnect.edu.au>
2021-10-07 14:57:57 +1000

    Swapped to new interactions

--- source/core/src/main/com/deco2800/game/entities/components/interactions/Actions/PlaceableBoxActions.java
+++ source/core/src/main/com/deco2800/game/entities/components/interactions/Actions/PlaceableBoxActions.java
@@ -24,8 +24,7 @@ public class PlaceableBoxActions extends InteractionComponent {
     }
 
     @Override
-    public void onCollisionStart(Fixture me, Fixture other) {
-        Entity target = preCollisionCheck(me, other);
+    public void onCollisionStart(Entity target) {
         if (target != null && target.getComponent(PlayerActions.class) != null) {
             logger.info("BOX started collision with PLAYER");
             highlightBox();
@@ -33,8 +32,7 @@ public class PlaceableBoxActions extends InteractionComponent {
     }
 
     @Override
-    public void onCollisionEnd(Fixture me, Fixture other) {
-        Entity target = preCollisionCheck(me, other);
+    public void onCollisionEnd(Entity target) {
         if (target != null && target.getComponent(PlayerActions.class) != null) {
             logger.info("BOX ended collision with PLAYER");
             deHighlightBox();
@@ -74,6 +72,5 @@ public class PlaceableBoxActions extends InteractionComponent {
         else {
             animator.startAnimation("box");
         }
-        this.getEntity().setPosition(100f,100f);
     }
 }
